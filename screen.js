5dec0e92f6e6154e01e830c02838d4ee5b0bda39
Sakib <najmuskg@gmail.com>
2022-06-26 22:37:44 +0600

    Added Some file

--- screen.js
+++ screen.js
@@ -3,3 +3,22 @@ window.addEventListener("load",() => {
     preload.classList.add("preload-finish");
 })
 
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction:false,
+    },
+});
+
+// var swiper = new Swiper('.swiper--container', {
+//     direction: 'vertical',
+//     slidesPerView: 1,
+//     // loop: true,
+//     autoplay: {
+//         delay: 1500,
+//         disableOnInteraction:false,
+//     },
+//   });
